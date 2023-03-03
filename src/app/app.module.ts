b63aaeec7c54d8e29c2ec6f9028aabf5733799b0
eric <eric.chee@microlise.com>
2023-03-03 20:29:06 +0000

    added emailjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ContactPageComponent } from './components/pages/contact-page/contact-page.component';
-import { CvPageComponent } from './components/pages/cv-page/cv-page.component';
-import { ProjectPageComponent } from './components/pages/project-page/project-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { ContactPageComponent } from "./components/pages/contact-page/contact-page.component";
+import { CvPageComponent } from "./components/pages/cv-page/cv-page.component";
+import { ProjectPageComponent } from "./components/pages/project-page/project-page.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,14 +20,15 @@ import { ProjectPageComponent } from './components/pages/project-page/project-pa
     FooterComponent,
     ContactPageComponent,
     CvPageComponent,
-    ProjectPageComponent
+    ProjectPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/pages/contact-page/contact-page.component.ts
+++ src/app/components/pages/contact-page/contact-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact-page',
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  selector: "app-contact-page",
+  templateUrl: "./contact-page.component.html",
+  styleUrls: ["./contact-page.component.css"],
 })
 export class ContactPageComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: "",
+      email: "",
+      message: "",
+    });
   }
 
+  public sendEmail(e: Event) {
+    emailjs
+      .sendForm(
+        "service_swls37v",
+        "template_ycenb63",
+        e.target as HTMLFormElement,
+        "VqHhh3VLEnwytN5bx"
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          this.form.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 }
